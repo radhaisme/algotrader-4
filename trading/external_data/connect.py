369ef47f6a773fd7bb8beee0bd9200066ca504ba
Javier <javier.macro.trader@gmail.com>
2017-12-29 14:27:57 -0500

    0.0.1

--- trading/external_data/connect.py
+++ trading/external_data/connect.py
@@ -9,9 +9,10 @@ from common import config
 import quandl
 
 
-def time_series(ticket, column='', sdate='', edate=''):
-    full_ticket = ticket + '.' + str(column)    
-    ans = quandl.get(full_ticket, start_date=sdate, end_date=edate)
+def time_series(database, dataset, column, sdate='', edate=''):
+    #full_ticket = ticket + '.' + str(column)    
+    ans = quandl.get(database_code=database, dataset_code=dataset, column_index = column, 
+                     start_date=sdate, end_date=edate)
     return ans
 
 
@@ -20,9 +21,10 @@ if __name__ == '__main__':
     my_quandl_api = config(section='DEFAULT', key='quandl_api_key')
     quandl.ApiConfig.api_key = my_quandl_api
     
-    my_ticket = 'USTREASURY/YIELD'
+    database = 'USTREASURY'
+    dataset = 'YIELD'
     col = 10
-    my_data = time_series(my_ticket, col)
+    my_data = time_series(database= database, dataset=dataset, column=col)
 
     print(my_data)
     
