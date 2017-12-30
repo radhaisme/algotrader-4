26047f55d3c05fba48883e16310f1ffc0f0f7128
Javier <javier.macro.trader@gmail.com>
2017-12-30 16:47:24 -0500

    database update

--- bsk_trading/common/__init__.py
+++ bsk_trading/common/__init__.py
@@ -12,3 +12,13 @@ def config(section, key, key_type):
     elif KeyError == 'bool':
         return c.getboolean(section, key)
 
+
+def mysql_config():
+    return {'dialect':config(section='MYSQL', key='dialect', key_type='str'),
+            'conector':config(section='MYSQL', key='conector', key_type='str'),
+            'server':config(section='MYSQL', key='server', key_type='str'),
+            'port':config(section='MYSQL', key='port', key_type='str'),
+            'user':config(section='MYSQL', key='user', key_type='str'),
+            'psw':config(section='MYSQL', key='password', key_type='str'),
+            'dbname':config(section='MYSQL', key='dbname', key_type='str'),
+            'echo':config(section='MYSQL', key='echo', key_type='bool')}
