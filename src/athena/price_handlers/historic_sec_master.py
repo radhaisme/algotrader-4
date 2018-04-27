6eb0be2da435f58a6a54b1ddf80133b947383dcc
Javier <javier.macro.trader@gmail.com>
2018-04-27 18:14:30 -0500

    Implement Influx DB

--- src/athena/price_handlers/historic_sec_master.py
+++ src/athena/price_handlers/historic_sec_master.py
@@ -14,47 +14,74 @@ class HistoricTickPriceHandler(AbstractTickPriceHandler):
 
     """
 
-    def __init__(self, events_queue, db_client, symbol_list, data_provider, start_time, end_time):
+    def __init__(self, events_queue, db_client, symbols_list, data_provider, start_time, end_time):
         self.db_client = db_client
         self.events_queue = events_queue
-        self.symbol_list = symbol_list
+        self.symbols_list = symbols_list
         self.data_provider = data_provider
         self.start_time = start_time
         self.end_time = end_time
         self.symbol_data = {}
 
-        self._get_the_data()
+        self._query_the_data(self.symbols_list, self.start_time, self.end_time)
 
-    def _query_one_symbol(self, symbol, s_time, e_time):
-        table = 'fx_tick'
-        q_statement = "SELECT *::field FROM \"{}\" WHERE provider=\'{}\' AND " \
-                      "symbol=\'{}\' AND time >= \'{}\' AND time < \'{}\'".format(table,
-                                                                                  self.data_provider,
-                                                                                  symbol,
-                                                                                  s_time,
-                                                                                  e_time)
-        print(q_statement)
+    def _query_the_data(self, symbols_list, s_time, e_time, table='fx_tick'):
+        """
+        Get tick data for a selected symbol over a period of time
+        Args:
+            symbols_list: the symbols to query about
+            s_time: start datetime object/string
+            e_time: end datetime object/string
+            table: the measurement in the influx database
+
+        Returns:
+
+        """
+
+        symbol_str = "AND (symbol=\'{}\' OR symbol=\'{}\')".format(symbols_list[0], symbols_list[1])
+
+        q_statement1 = "SELECT * " \
+                       "FROM \"{}\" " \
+                       "WHERE provider=\'{}\' " \
+                       "AND time >= \'{}\' " \
+                       "AND time < \'{}\' ".format(table,
+                                                   self.data_provider,
+                                                   s_time,
+                                                   e_time)
+
+
+
+        final_statement = q_statement1 + " " + symbol_str
+        print(final_statement)
         clients = self.db_client
-        rs = clients.query(q_statement)
+        rs = clients.query(final_statement)
+        print(rs)
         return rs.get_points()
 
-    def _get_the_data(self):
-        for each_symbol in symbols:
-            self.symbol_data[each_symbol] = self._query_one_symbol(symbol=each_symbol,
-                                                                   s_time=self.start_time,
-                                                                   e_time=self.end_time)
 
+    def get_symbol_ticks(self, symbol):
+        """
+        Tick data for one of the symbols in the input list
+        Args:
+            symbol:
+
+        Returns: tick price generator
+
+        """
+        return self.symbol_data[symbol]
+
+    def get_all_sync__ticks(self):
+        pass
 
 
 if __name__ == '__main__':
 
     my_queue = queue.Queue()
     client = influx_client()
-    symbols = ['EURUSD', 'GBPUSD']
+    symbols = ['EURUSD', 'AUDCAD', 'USDJPY']
     provider = 'fxcm'
-    s_date = '2014-12-28 00:22:00.000'
-    e_date = '2014-12-28 00:22:05.000'
+    s_date = '2018-02-01 15:01:00.000'
+    e_date = '2018-02-01 15:01:01.000'
 
     x = HistoricTickPriceHandler(my_queue, client, symbols, provider, s_date, e_date)
 
-    print(x)
